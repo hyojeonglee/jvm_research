c4c2aa1a746a7ada0d291d81bf750ecbe19128cd
Hyojeong Lee <hyojeong5663@gmail.com>
2018-08-21 15:30:51 +0900

    Clean directory

--- jvm+swap_dapa/pageidle.h
+++
@@ -1,6 +0,0 @@
-typedef unsigned long long u8;
-
-void setidle(int nr_pfns, u8 pfns[]);
-int *getidle(int nr_pfns, u8 pfns[]);
-u8 nr_active(int nr_pfns, u8 pfns[]);
-u8 sz_active(int nr_pfn, u8 pfns[], int is_huge[]);
